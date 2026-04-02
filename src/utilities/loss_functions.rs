0e75fdac4f13a102e3268cad4fbc0075d3c5f5c9
Rem Sadykhov <rsadykhov@gmail.com>
2026-04-02 15:10:34 +0100

    Updated Loss Functions

--- src/utilities/loss_functions.rs
+++ src/utilities/loss_functions.rs
@@ -23,49 +23,11 @@ pub trait LossFunction {
     /// - `predictions`: An iterator of values predicted by the model
     ///
     /// # Output
-    /// - Error/losse
+    /// - Error/loss
     fn loss_iter<T, I>(&self, observations: T, predictions: T) -> Result<f64, DigiFiError>
     where
         T: Iterator<Item = I> + ExactSizeIterator,
         I: Borrow<f64>;
-}
-
-
-/// Measures an error between paired observations (Usually, empirical observations vs simulated observations).
-///
-/// # LaTeX Formula
-/// - MAE = \\frac{\\sum^{n}\_{i=1}\\lvert y_{i}-x_{i}\\rvert}{n}
-///
-/// # Links
-/// - Wikipedia: <https://en.wikipedia.org/wiki/Mean_absolute_error>
-/// - Original Source: N/A
-pub struct MAE;
-
-impl LossFunction for MAE {
-    fn loss(&self, observation: f64, prediction: f64) -> f64 {
-        (observation - prediction).abs()
-    }
-
-    /// Measures an error between observed and predicted values.
-    ///
-    /// # Input
-    /// - `observations`: An iterator of observed/empirical values
-    /// - `predictions`: An iterator of values predicted by the model
-    ///
-    /// # Output
-    /// - Error/losse
-    ///
-    /// # Errors
-    /// - Returns an error if the lengths of `observations` and `predictions` do not coincide.
-    fn loss_iter<T, I>(&self, observations: T, predictions: T) -> Result<f64, DigiFiError>
-    where
-        T: Iterator<Item = I> + ExactSizeIterator,
-        I: Borrow<f64>,
-    {
-        compare_len(&observations, &predictions, "observations", "predictions")?;
-        let len: f64 = observations.len() as f64;
-        Ok(zip(observations, predictions).fold(0.0, |sum, (o, p)| { sum + (o.borrow() - p.borrow()).abs() } ) / len)
-    }
 }
 
 
@@ -91,7 +53,7 @@ impl LossFunction for MSE {
     /// - `predictions`: An iterator of values predicted by the model
     ///
     /// # Output
-    /// - Error/losse
+    /// - Error/loss
     ///
     /// # Errors
     /// - Returns an error if the lengths of `observations` and `predictions` do not coincide.
@@ -110,10 +72,124 @@ impl LossFunction for MSE {
 /// Measures an error between paired observations (Usually, empirical observations vs simulated observations).
 ///
 /// # LaTeX Formula
-/// - MSE = \\sum^{n}\_{i=1}(y_{i}-x_{i})^{2}
+/// - MAE = \\frac{\\sum^{n}\_{i=1}\\lvert y_{i}-x_{i}\\rvert}{n}
+///
+/// # Links
+/// - Wikipedia: <https://en.wikipedia.org/wiki/Mean_absolute_error>
+/// - Original Source: N/A
+pub struct MAE;
+
+impl LossFunction for MAE {
+    fn loss(&self, observation: f64, prediction: f64) -> f64 {
+        (observation - prediction).abs()
+    }
+
+    /// Measures an error between observed and predicted values.
+    ///
+    /// # Input
+    /// - `observations`: An iterator of observed/empirical values
+    /// - `predictions`: An iterator of values predicted by the model
+    ///
+    /// # Output
+    /// - Error/loss
+    ///
+    /// # Errors
+    /// - Returns an error if the lengths of `observations` and `predictions` do not coincide.
+    fn loss_iter<T, I>(&self, observations: T, predictions: T) -> Result<f64, DigiFiError>
+    where
+        T: Iterator<Item = I> + ExactSizeIterator,
+        I: Borrow<f64>,
+    {
+        compare_len(&observations, &predictions, "observations", "predictions")?;
+        let len: f64 = observations.len() as f64;
+        Ok(zip(observations, predictions).fold(0.0, |sum, (o, p)| { sum + (o.borrow() - p.borrow()).abs() } ) / len)
+    }
+}
+
+
+/// Measures an error between paired observations (Usually, empirical observations vs simulated observations).
+///
+/// # LaTeX Formula
+/// - MSLE = \\frac{1}{n}\\sum^{n}\_{i=1}(ln(\\frac{y_{i}+1}{x_{i} + 1}))^{2}
 ///
 /// # Links
 /// - Wikipedia: N/A
+/// - Original Source: N/A
+pub struct MSLE;
+
+impl LossFunction for MSLE {
+    fn loss(&self, observation: f64, prediction: f64) -> f64 {
+        ((observation + 1.0) - (prediction + 1.0)).ln().powi(2)
+    }
+
+    /// Measures an error between observed and predicted values.
+    ///
+    /// # Input
+    /// - `observations`: An iterator of observed/empirical values
+    /// - `predictions`: An iterator of values predicted by the model
+    ///
+    /// # Output
+    /// - Error/loss
+    ///
+    /// # Errors
+    /// - Returns an error if the lengths of `observations` and `predictions` do not coincide.
+    fn loss_iter<T, I>(&self, observations: T, predictions: T) -> Result<f64, DigiFiError>
+    where
+        T: Iterator<Item = I> + ExactSizeIterator,
+        I: Borrow<f64>,
+    {
+        compare_len(&observations, &predictions, "observations", "predictions")?;
+        let len: f64 = observations.len() as f64;
+        Ok(zip(observations, predictions).fold(0.0, |sum, (o, p)| { sum + ((o.borrow() + 1.0) / (p.borrow() + 1.0)).ln().powi(2) } ) / len)
+    }
+}
+
+
+/// Measures an error between paired observations (Usually, empirical observations vs simulated observations).
+///
+/// # LaTeX Formula
+/// - MLE = \\sqrt{\\frac{1}{n}\\sum^{n}\_{i=1}(ln(\\frac{y_{i}+1}{x_{i} + 1}))^{2}}
+///
+/// # Links
+/// - Wikipedia: N/A
+/// - Original Source: N/A
+pub struct MLE;
+
+impl LossFunction for MLE {
+    fn loss(&self, observation: f64, prediction: f64) -> f64 {
+        ((observation + 1.0) - (prediction + 1.0)).ln().abs()
+    }
+
+    /// Measures an error between observed and predicted values.
+    ///
+    /// # Input
+    /// - `observations`: An iterator of observed/empirical values
+    /// - `predictions`: An iterator of values predicted by the model
+    ///
+    /// # Output
+    /// - Error/loss
+    ///
+    /// # Errors
+    /// - Returns an error if the lengths of `observations` and `predictions` do not coincide.
+    fn loss_iter<T, I>(&self, observations: T, predictions: T) -> Result<f64, DigiFiError>
+    where
+        T: Iterator<Item = I> + ExactSizeIterator,
+        I: Borrow<f64>,
+    {
+        compare_len(&observations, &predictions, "observations", "predictions")?;
+        let msle: MSLE = MSLE;
+        Ok(msle.loss_iter(observations, predictions)?.powf(0.5))
+    }
+}
+
+
+/// Measures an error between paired observations (Usually, empirical observations vs simulated observations).
+///
+/// # LaTeX Formula
+/// - SSE = \\sum^{n}\_{i=1}(y_{i}-x_{i})^{2}
+///
+/// # Links
+/// - Wikipedia: <https://en.wikipedia.org/wiki/Residual_sum_of_squares>
 /// - Original Source: N/A
 pub struct SSE;
 
@@ -129,7 +205,7 @@ impl LossFunction for SSE {
     /// - `predictions`: An iterator of values predicted by the model
     ///
     /// # Output
-    /// - Error/losse
+    /// - Error/loss
     ///
     /// # Errors
     /// - Returns an error if the lengths of `observations` and `predictions` do not coincide.
@@ -166,7 +242,7 @@ impl LossFunction for StraddleLoss {
     /// - `predictions`: An iterator of values predicted by the model
     ///
     /// # Output
-    /// - Error/losse
+    /// - Error/loss
     ///
     /// # Errors
     /// - Returns an error if the lengths of `observations` and `predictions` do not coincide.
